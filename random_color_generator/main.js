e7d323590532d0c17d1db93a2689fa636dbd5c27
Dalaikhvv <dd4184968@gmail.com>
2022-12-28 19:17:55 +0800

    12.28

--- random_color_generator/main.js
+++ random_color_generator/main.js
@@ -1,1 +1,24 @@
-document.getElementById("myBtn").onclick;
+const button = document.querySelector("#btn");
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const bc = document.querySelector("body");
+
+function changeBackgroundColor() {
+  const green = getRandomNumber(0, 255);
+  const red = getRandomNumber(0, 255);
+  const blue = getRandomNumber(0, 255);
+
+  bc.style.backgroundColor = `rgb(${green},${red},${blue})`;
+}
+function changeButtonColor() {
+  const green = getRandomNumber(0, 255);
+  const red = getRandomNumber(0, 255);
+  const blue = getRandomNumber(0, 255);
+
+  button.style.backgroundColor = `rgb(${green},${red},${blue})`;
+}
+
+button.addEventListener("click", changeBackgroundColor);
+button.addEventListener("click", changeButtonColor);
